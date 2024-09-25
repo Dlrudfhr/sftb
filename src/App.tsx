0a8ccef70b377517afbcf2c14f395f037425c21d
seosujin <sytolo020912@gmail.com>
2024-09-25 09:41:02 +0900

    서수진

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import SearchIdPage from "./pages/SearchIdPage";
 import SearchPwPage from "./pages/SearchPwPage";
 import PostWrite from "./pages/PostPage/PostWrite";
 import PrivateRoute from "./PrivateRoute";
+import PostDetail from "./pages/PostPage/PostDetail";
 import "./App.css";
 
 interface RouteProps {
@@ -55,6 +56,7 @@ const routes: Array<RouteProps> = [
   { path: "/Ledger", component: Ledger },
   { path: "/FreePost", component: FreePost },
   { path: "/PostWrite", component: PostWrite },
+  { path: "/PostDetail", component: PostDetail },
 ];
 
 function App() {
