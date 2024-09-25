6f768fbdd1f30edf6209438257c1edc841317082
weon1013 <x147x147@naver.com>
2024-09-25 17:16:14 +0900

    2024-09-25 게시물 작성 최종본

--- src/pages/PostPage/PostWrite.tsx
+++ src/pages/PostPage/PostWrite.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { useState } from "react";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+// PostWrite.js
+
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 import "../../assets/css/PostWrite.css";
@@ -17,7 +18,7 @@ function PostWrite() {
   console.log("userName from localStorage:", userName); // userName 로그 출력
 
   // 게시물 제출 함수
-  const handleSubmit = async (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // 폼 제출 시 페이지 리로드 방지
 
     try {
@@ -34,7 +35,8 @@ function PostWrite() {
         {
           title: title,
           content: content,
-          userName: userName, // userName을 memberId로 사용
+          userName: userName, // userName을 Member_ID로 사용
+          boardId: 2, // 자격증 정보 게시판의 ID를 추가
         },
         {
           headers: {
@@ -61,8 +63,7 @@ function PostWrite() {
         <form className="postWrite__form" onSubmit={handleSubmit}>
           <h3 className="postpage_title" onClick={() => (window.location.href = "/Certificate")}>게시물 작성</h3>
 
-          <label className="postWrite__label" htmlFor="post_title">
-          </label>
+          <label className="postWrite__label" htmlFor="post_title"></label>
           <input
             className="post_title"
             type="text"
@@ -74,8 +75,7 @@ function PostWrite() {
           />
           <br />
 
-          <label className="postWrite__label" htmlFor="post_content">
-          </label>
+          <label className="postWrite__label" htmlFor="post_content"></label>
           <textarea
             className="post_textarea"
             id="post_content"
