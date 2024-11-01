f174d5eabb1d4fe13e597ab550274895ecbb6978
Gyeongrok Lee <20rudfhr@naver.com>
2024-11-01 19:42:36 +0900

    20241101_이경록

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -7,14 +7,12 @@ const Store: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div>
       <Header /> {/* 상단 배너 */}
-        
       <Footer /> {/* 하단 배너 */}
       <div className="Store__background">
         <div className="Store__section">
           <div className="Store__h1">_</div>
           <div className="Store__frame">
             <div className="Store__innerFrame">
-
               <div className="Store__productLine1">
                 <div className="Store__prodBlock">
                   <div className="Store__product"></div>
@@ -33,7 +31,7 @@ const Store: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                   <div className="Store__selectBtn">SELECT</div>
                 </div>
               </div>
-              
+
               <div className="Store__productLine2">
                 <div className="Store__prodBlock">
                   <div className="Store__product"></div>
@@ -51,12 +49,8 @@ const Store: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                   <div className="Store__product"></div>
                   <div className="Store__selectBtn">SELECT</div>
                 </div>
-
-                
               </div>
-              <div className="Store__coin">
-                1,000 coin
-              </div>
+              <div className="Store__coin">1,000 coin</div>
               <div className=""></div>
             </div>
           </div>
