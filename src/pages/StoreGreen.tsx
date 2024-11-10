0070e925cd7011624e9b6c3f0b1a9860b8443b9f
jeong yi <jeongyi0131@naver.com>
2024-11-10 22:01:43 +0900

    프론트 병합2

--- src/pages/StoreGreen.tsx
+++ src/pages/StoreGreen.tsx
@@ -8,6 +8,8 @@ import star from "../assets/images/기프티콘사진/star_americano.jpg";
 import greenBtn from "../assets/images/greenBtn.png"; // 빨간 버튼 이미지
 import redBtn from "../assets/images/redBtn.png"; // 빨간 버튼 이미지
 import rightBtn from "../assets/images/rightBtn.png";
+import coffee from "../assets/images/자산 1.png";
+import cake from "../assets/images/자산 2.png";
 
 const StoreGreen: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [tokenCount, setTokenCount] = useState<number | null>(null);
@@ -42,78 +44,87 @@ const StoreGreen: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <Header />
       <Footer />
       <div className="StoreGreen__background">
-        <button className="StoreGreen__PrevBtn" onClick={goToPrevStore}>
-          ◀
-        </button>
-
-        <div className="StoreGreen__frame">
-          <div className="StoreGreen__innerFrame">
-            <div className="StoreGreen__productLine1">
-              <img src={star} className="StoreGreen__product" alt="스타 상품" />
-              <div className="StoreGreen__product"></div>
-              <div className="StoreGreen__product"></div>
-              <div className="StoreGreen__product"></div>
-            </div>
-            <div className="StoreGreen__selectBtnLine1">
-              {/* 
-              1 2 3 4
-              5 6 7 8 상품순
-              */}
-              {/*첫번째 상품 가격, 두번째 상품 가격, 세번째 상품가격, 네번째 상품가격 */}
-              {[10000, 10001, 9999, 10002].map((value, index) => (
-                <button
-                  key={index}
-                  className={`StoreGreen__selectBtn${index + 1}`}
-                  type="button"
-                  value={value}
-                  style={{
-                    backgroundImage: `url(${
-                      tokenCount !== null && tokenCount >= value
-                        ? greenBtn
-                        : redBtn
-                    })`,
-                    backgroundSize: "cover",
-                    backgroundColor: "transparent",
-                  }}
-                />
-              ))}
-            </div>
-            <div className="StoreGreen__productLine2">
-              <div className="StoreGreen__product"></div>
-              <div className="StoreGreen__product"></div>
-              <div className="StoreGreen__product"></div>
-              <div className="StoreGreen__product"></div>
-            </div>
-            <div className="StoreGreen__selectBtnLine2">
-              {[10000, 10001, 9999, 10002].map((value, index) => (
-                <button
-                  key={index}
-                  className={`StoreGreen__selectBtn${index + 5}`}
-                  type="button"
-                  value={value}
-                  style={{
-                    backgroundImage: `url(${
-                      tokenCount !== null && tokenCount >= value
-                        ? greenBtn
-                        : redBtn
-                    })`,
-                    backgroundSize: "cover",
-                    backgroundColor: "transparent",
-                  }}
-                />
-              ))}
-            </div>
-            <div className="StoreGreen__coin">
-              {tokenCount !== null && tokenCount !== undefined
-                ? `${tokenCount.toLocaleString()}`
-                : "로딩 중..."}
-              <div className="StoreGreen__cointext">coin</div>
+        <div className="Store__area">
+          <div className="Store__left">
+            <button className="StoreGreen__PrevBtn" onClick={goToPrevStore}>
+              ◀
+            </button>
+            <img src={coffee} className="Store__move"></img>
+            <img src={cake} className="Store__move"></img>
+          </div>
+          <div className="StoreGreen__middle">
+            <div className="StoreGreen__innerFrame">
+              <div className="StoreGreen__productLine1">
+                <img src={star} className="StoreGreen__product" alt="스타 상품" />
+                <div className="StoreGreen__product"></div>
+                <div className="StoreGreen__product"></div>
+                <div className="StoreGreen__product"></div>
+              </div>
+              <div className="StoreGreen__selectBtnLine1">
+                {/* 
+                1 2 3 4
+                5 6 7 8 상품순
+                */}
+                {/*첫번째 상품 가격, 두번째 상품 가격, 세번째 상품가격, 네번째 상품가격 */}
+                {[10000, 10001, 9999, 10002].map((value, index) => (
+                  <button
+                    key={index}
+                    className={`StoreGreen__selectBtn${index + 1}`}
+                    type="button"
+                    value={value}
+                    style={{
+                      backgroundImage: `url(${
+                        tokenCount !== null && tokenCount >= value
+                          ? greenBtn
+                          : redBtn
+                      })`,
+                      backgroundSize: "cover",
+                      backgroundColor: "transparent",
+                    }}
+                  />
+                ))}
+              </div>
+              <div className="StoreGreen__productLine2">
+                <div className="StoreGreen__product"></div>
+                <div className="StoreGreen__product"></div>
+                <div className="StoreGreen__product"></div>
+                <div className="StoreGreen__product"></div>
+              </div>
+              <div className="StoreGreen__selectBtnLine2">
+                {[10000, 10001, 9999, 10002].map((value, index) => (
+                  <button
+                    key={index}
+                    className={`StoreGreen__selectBtn${index + 5}`}
+                    type="button"
+                    value={value}
+                    style={{
+                      backgroundImage: `url(${
+                        tokenCount !== null && tokenCount >= value
+                          ? greenBtn
+                          : redBtn
+                      })`,
+                      backgroundSize: "cover",
+                      backgroundColor: "transparent",
+                    }}
+                  />
+                ))}
+              </div>
+              <div className="StoreGreen__coin">
+                {tokenCount !== null && tokenCount !== undefined
+                  ? `${tokenCount.toLocaleString()}`
+                  : "로딩 중..."}
+                <div className="StoreGreen__cointext">coin</div>
+              </div>
             </div>
           </div>
+          <div className="Store__right">
+            <button className="StoreGreen__NextBtn" onClick={goToNextStore}>
+              <img src={rightBtn}></img>
+            </button>
+
+          </div>
         </div>
-        <button className="StoreGreen__NextBtn" onClick={goToNextStore}>
-          <img src={rightBtn}></img>
-        </button>
+
       </div>
     </div>
   );
