a0297e2e0e77827df748509c04fdd193b77f8514
Chanhoe <cksghl7979@naver.com>
2024-11-18 18:47:08 +0900

    11/18 찬회 마이페이지 정보 불러오기

--- src/pages/Information.tsx
+++ src/pages/Information.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import Header from "./Header"; // 상단 배너 컴포넌트
 import Footer from "./Footer"; // 하단 배너 컴포넌트
+import axios from 'axios';
 import "../assets/css/Information.css";
 
 const Information: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<any>(null);
+  const userId = localStorage.getItem("memberId");
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/auth/users/info`, {
+          params: { userId: userId },
+        });
+        console.log(response.data);
+        setUser(response.data); // 사용자 정보를 상태에 저장
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      } 
+    };
+
+    fetchUserInfo();
+  }, []); // userId가 변경될 때마다 호출
+
   return (
     <div>
       <Header /> {/* 상단 배너 */}
@@ -16,7 +35,7 @@ const Information: React.FC<{ children: React.ReactNode }> = ({ children }) => {
               
               <div className="Information__userImage"><img></img></div>
               <div className="Information__user">
-                <div className="Information__userInfo">NAME: 사용자 이름</div>
+                <div className="Information__userInfo">NAME: 사용자 이름{user.userName}</div>
                 <div className="Information__userInfo">학번:</div>
                 <div className="Information__userInfo">TIER: 사용자 티어</div>
                 <div className="Information__userInfo">LEVEL: 사용자 레벨</div>
